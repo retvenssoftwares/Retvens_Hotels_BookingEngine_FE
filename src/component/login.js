61361c344de767bdd284b3fa44a19a0f1e0d4f1f
Rahul WorkPlace <rahul.bhalerao@retvensservices.com>
2023-10-13 15:49:00 +0530

    update

--- src/component/login.js
+++ src/component/login.js
@@ -5,13 +5,7 @@ export default function Auth() {
 
   const [isLogin, setIsLogin] = useState(true)
 
-  // useEffect(()=>{
-  //     if(isLogin){
-  //         alert("Login")
-  //     }else{
-  //         alert('register')
-  //     }
-  // },[isLogin])
+
 
   return (
     <>
@@ -35,6 +29,51 @@ export default function Auth() {
 }
 
 const Register = () => {
+  
+  const handleSignup = () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mobile = document.getElementById("mobile").value;
+    const password =document.getElementById("password").value
+   const repassword =document.getElementById("repassword").value
+    // Create a data object with user registration information
+
+     // Check if the passwords match
+     if (password !== repassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    const userData = {
+     firstName: name,
+      email:email,
+      phoneNumber:mobile,
+      pwd:password,
+    
+    };
+
+    // Make a POST request to the signup API using the fetch API
+    fetch("http://localhost:9000/signUp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Registration was successful
+          alert("Registration successful");
+        } else {
+          // Registration failed, handle the error
+          alert("Registration failed");
+        }
+      })
+      .catch((error) => {
+        // Handle any network errors
+        alert("Network error: " + error.message);
+      });
+  };
   return <>
 
     <div class="container-fluid bg-light p-0">
@@ -46,114 +85,77 @@ const Register = () => {
             <form>
               <div class="form-group">
                 <label for="name">Name:</label>
-                <input type="text" class="form-control" />
+                <input id="name" type="text" class="form-control" />
               </div>
               <br />
               <div class="form-group">
                 <label for="email">Email address:</label>
-                <input type="email" class="form-control" />
-              </div>
-              <br />
-              <div class="form-group">
-                <label for="pwd">Password:</label>
-                <input type="password" class="form-control" />
-              </div>
-              <br />
-              <div class="form-group">
-                <label for="repwd">Re-enter Password:</label>
-                <input type="password" class="form-control" />
+                <input id="email" type="email" class="form-control" />
               </div>
               <br />
               <div class="form-group">
                 <label for="mobile">Mobile:</label>
-                <input type="text" class="form-control" />
+                <input id="mobile" type="text" class="form-control" />
+              </div>
+              <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input id="password" type="password" class="form-control" />
               </div>
               <br />
+              <div class="form-group">
+                <label for="repwd">Re-enter Password:</label>
+                <input id="repassword" type="password" class="form-control" />
+              </div>
+              <br />
+            
+              <br />
               <p>Already have an account? <a href="#">Login</a></p>
-              <button type="button" class="btn btn-success">Submit</button>
+              <button type="button" class="btn btn-success" onClick={handleSignup}>Submit</button>
             </form>
           </div>
         </div>
       </div>
     </div>
-
-    {/* <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <form>
-              <h2 className=" justify-content-center">Register</h2>
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email Address</label>
-                <input type="email" className="form-control" id="email" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="reenterPassword">Re-enter Password</label>
-                <input type="password" className="form-control" id="reenterPassword" />
-              </div>
-              <button type="submit" className="btn btn-primary">Register</button>
-              <p>Already have an account? <a href="#">Login</a></p>
-            </form>
-          </div>
-        </div>
-      </div> */}
-
-    {/* <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <form>
-              <h2>Register</h2>
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email Address</label>
-                <input type="email" className="form-control" id="email" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="reenterPassword">Re-enter Password</label>
-                <input type="password" className="form-control" id="reenterPassword" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="mobile">Mobile</label>
-                <input type="text" className="form-control" id="mobile" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="city">City</label>
-                <input type="text" className="form-control" id="city" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="gender">Gender</label>
-                <select className="form-control" id="gender">
-                  <option value="">Select</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-              <button type="submit" className="btn btn-primary">Register</button>
-              <p>Already have an account? <a href="#">Login</a></p>
-            </form>
-          </div>
-        </div>
-      </div> */}
-
   </>
 }
 
 const Login = () => {
+  const handleLogin = () => {
+
+    const email = document.getElementById("email").value;
+
+    const password =document.getElementById("password").value
+
+    // Create a data object with user registration information
+
+    const userData = {
+      email:email,
+      passwd:password,
+    
+    };
+
+    // Make a POST request to the signup API using the fetch API
+    fetch("http://localhost:9000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Registration was successful
+          alert("login successful");
+        } else {
+          // Registration failed, handle the error
+          alert("login failed");
+        }
+      })
+      .catch((error) => {
+        // Handle any network errors
+        alert("Network error: " + error.message);
+      });
+  };
   return <>
     <div class="container-fluid bg-light p-0">
       <div class="row g-0">
@@ -165,16 +167,16 @@ const Login = () => {
               <br />
               <div class="form-group">
                 <label for="email">Email address:</label>
-                <input type="email" class="form-control" />
+                <input id="email" type="email" class="form-control" />
               </div>
               <br />
               <div class="form-group">
                 <label for="pwd">Password:</label>
-                <input type="password" class="form-control" />
+                <input id="password" type="password" class="form-control" />
               </div>
               <br />
               <p>Don't have an account? <a href="#">Register here</a></p>
-              <button type="button" class="btn btn-success">Submit</button>
+              <button type="button" class="btn btn-success" onClick={handleLogin}>Submit</button>
             </form>
           </div>
         </div>
